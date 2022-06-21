764d8e256a03c71e2995bd9f561b017b0d96e9b2
Abhishek Kumar Tiwari <abhishek.tiwari@opstree.com>
2022-06-21 12:11:35 +0530

    Add k8s python libs

--- otk8slibs/k8s_deployment_actions_factory.py
+++ otk8slibs/k8s_deployment_actions_factory.py
@@ -1,15 +1,4 @@
-import  logging
-import schedule_resource_logger
 from kubernetes import client, config
-from botocore.exceptions import ClientError
-
-
-SCHEDULE_ACTION_ENV_KEY = "K8s_SCHEDULE_ACTION"
-LOG_PATH = "/var/log/ot/aws-resource-scheduler.log"
-
-
-LOGGER = schedule_resource_logger._get_logging(LOG_PATH)
-
 
 class k8sActions:
     def __init__(self, cli):
@@ -30,20 +19,7 @@ class k8sActions:
                 pass
         return deployments
 
-    def statefulset_having_annotation(self, namespace, sts_annot):
-        self.namespace = namespace
-        self.sts_annot = sts_annot
 
-        statefulset = []
-        annotation = list(self.sts_annot)
-        for x in range(len(self.cli.list_namespaced_stateful_set(namespace=self.namespace).items)):
-            try:
-                if self.cli.list_namespaced_stateful_set(namespace=self.namespace).items[x].metadata.annotations[annotation[0]] == "true" :
-                    statefulset.append(self.cli.list_namespaced_stateful_set(
-                        namespace=self.namespace).items[x].metadata.name)
-            except:
-                pass
-        return statefulset
     
     def deployment_replica_change(self, properties, deployments, namespace, action):
         self.properties = properties
@@ -62,23 +38,6 @@ class k8sActions:
                     "spec": {"replicas": replicas, }}
             print(self.cli.patch_namespaced_deployment_scale(
                 namespace=self.namespace, name=deployment, body=body))
-
-    def statefulset_replica_change(self, properties, statefulset, namespace, action):
-        self.properties = properties
-        self.statefulset = statefulset
-        self.namespace = namespace
-        self.action = action
-
-        if self.action == "start": 
-            replicas = self.properties['k8s']['replicas']
-        else:
-            replicas = 0
-
-        for statefulset in self.statefulset:
-            body = {"apiVersion": "apps/v1", "kind": "StatefulSet",
-                    "spec": {"replicas": replicas, }}
-            print(self.cli.patch_namespaced_stateful_set_scale(
-                namespace=self.namespace, name=statefulset, body=body))
 
 
     
