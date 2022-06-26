b1f68df8ed374570cf772fbabeb01f1c1a0a95c8
Abhishek Kumar Tiwari <abhishek.tiwari@opstree.com>
2022-06-26 22:16:57 +0530

    Update the libs on the basis of MR suggestions

--- otk8slibs/k8s_deployment_factory.py
+++ otk8slibs/k8s_deployment_factory.py
@@ -1,34 +1,29 @@
-from kubernetes import client, config
-
-class k8sActions:
+class k8sDeploymentActions:
     def __init__(self, cli):
         self.cli = cli
 
-    def deployment_having_annotation(self, namespace, deployment_annot):
+    def _get_deployments_with_annotation(self, namespace, deployment_annot):
         self.namespace = namespace
         self.deployment_annot = deployment_annot
 
         deployments = []
-        annotation = list(self.deployment_annot)
-        for x in range(len(self.cli.list_namespaced_deployment(namespace=self.namespace).items)):
+        annotations = list(self.deployment_annot)
+        for deployment in range(len(self.cli.list_namespaced_deployment(namespace=self.namespace).items)):
             try:
-                if self.cli.list_namespaced_deployment(namespace=self.namespace).items[x].metadata.annotations[annotation[0]] == "true" :
+                if self.cli.list_namespaced_deployment(namespace=self.namespace).items[deployment].metadata.annotations[annotations[0]] == "true":
                     deployments.append(self.cli.list_namespaced_deployment(
-                        namespace=self.namespace).items[x].metadata.name)
+                        namespace=self.namespace).items[deployment].metadata.name)
             except:
                 pass
         return deployments
 
-
-    
-    def deployment_replica_change(self, properties, deployments, namespace, action):
+    def deployments_replica_change(self, properties, deployments, namespace, action):
         self.properties = properties
         self.deployments = deployments
         self.namespace = namespace
         self.action = action
 
-
-        if self.action == "start": 
+        if self.action == "start":
             replicas = self.properties['k8s']['replicas']
         else:
             replicas = 0
@@ -36,8 +31,5 @@ class k8sActions:
         for deployment in self.deployments:
             body = {"apiVersion": "apps/v1", "kind": "Deployment",
                     "spec": {"replicas": replicas, }}
-            print(self.cli.patch_namespaced_deployment_scale(
+            return(self.cli.patch_namespaced_deployment_scale(
                 namespace=self.namespace, name=deployment, body=body))
-
-
-    
--- otk8slibs/k8s_kube_session.py
+++ otk8slibs/k8s_kube_session.py
@@ -1,7 +1,7 @@
 from kubernetes import  config
 
 def _create_k8s_session(k8s_context=None):
-    
+
     if k8s_context:
         config.load_kube_config(context=k8s_context)
     else:
