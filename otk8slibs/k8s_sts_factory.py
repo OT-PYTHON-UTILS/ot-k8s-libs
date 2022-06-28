df0e946b0aee134997fe563e032573efc15478cd
Abhishek Kumar Tiwari <abhishek.tiwari@opstree.com>
2022-06-28 10:53:43 +0530

    Update the lib on the MR suggestions

---
+++ otk8slibs/k8s_sts_factory.py
@@ -0,0 +1,37 @@
+class k8sStsActions:
+    def __init__(self, cli):
+        self.cli = cli
+
+
+    def _get_statefulsets_with_annotation(self, namespace, sts_annot):
+        self.namespace = namespace
+        self.sts_annot = sts_annot
+
+        statefulsets = []
+        annotations = list(self.sts_annot)
+        for statefulset in range(len(self.cli.list_namespaced_stateful_set(namespace=self.namespace).items)):
+            try:
+                if self.cli.list_namespaced_stateful_set(namespace=self.namespace).items[statefulset].metadata.annotations[annotations[0]] == "true" :
+                    statefulsets.append(self.cli.list_namespaced_stateful_set(
+                        namespace=self.namespace).items[statefulset].metadata.name)
+            except:
+                pass
+        return statefulsets
+
+
+    def statefulsets_replica_change(self, properties, statefulsets, namespace, action):
+        self.properties = properties
+        self.statefulsets = statefulsets
+        self.namespace = namespace
+        self.action = action
+
+        if self.action == "start":
+            replicas = self.properties['k8s']['replicas']
+        else:
+            replicas = 0
+
+        for statefulset in self.statefulsets:
+            body = {"apiVersion": "apps/v1", "kind": "StatefulSet",
+                    "spec": {"replicas": replicas, }}
+            return(self.cli.patch_namespaced_stateful_set_scale(
+                namespace=self.namespace, name=statefulset, body=body))
